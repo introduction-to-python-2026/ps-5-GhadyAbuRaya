cead1f5b8ec81a0cf060bb6ef9ba5b2cf6ede238
GhadyAbuRaya <vxytjghx7d@privaterelay.appleid.com>
2025-12-09 17:03:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from string_utils import (
+    split_before_uppercases,
+    split_at_digit,
+    count_atoms_in_molecule,
+    parse_chemical_reaction,
+    count_atoms_in_reaction,
+)
+
+from equation_utils import build_equations, my_solve
+
+
+
+
+
 def balance_reaction(reaction):
     # 1. parse reaction
     reactants, products = parse_chemical_reaction(reaction)
@@ -6,8 +20,9 @@ def balance_reaction(reaction):
 
     # 2. build equations and solve
     equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = my_solve(equations, coefficients)
+    solved_coeffs = my_solve(equations, coefficients)
 
-    return coefficients
+    # 3. add the last product coefficient (which is fixed to 1 in build_equations)
+    return solved_coeffs + [1]
 
 
